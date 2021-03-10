14e3d8be51c48fb7cda472b8ed8ada5e6fcd74d1
Leopardo <72420215+L3opardo@users.noreply.github.com>
2021-03-09 22:26:31 -0300

    Add files via upload

---
+++ Covidometro/src/covidometro/Covidometro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package covidometro;
+
+/**
+ *
+ * @author SESI_SENAI
+ */
+public class Covidometro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
